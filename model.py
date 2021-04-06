05bc02d6cfd383ce222a72f9ff4865e31408834c
Luke Wassink <40555655+Wassl12@users.noreply.github.com>
2021-04-05 23:53:06 -0400

    Model additions.

--- model.py
+++ model.py
@@ -4,35 +4,42 @@ import torch.nn.functional as F
 import numpy as np
 
 class RNN(nn.Module):
-    def __init__(self, input_size, 2*roster_size, output_size):
+    """Going to pass Batch,Players,2,2"""
+
+    def __init__(self, num_weekly_features):
         super().__init__()
-        #TODO: fill in the definitions for each layer
-        self.hidden_size = hidden_size
-        self.lstm = nn.LSTMCell(input_size, hidden_size)
-        self.fc = nn.Linear(hidden_size,output_size)
+        self.hidden_size = 80
+        self.roster_scan_1 = nn.Conv2d(240, out_channels=240, kernel_size=1)
+        self.roster_scan_2 = nn.Conv2d(240, 80, kernel_size=2)
+        self.lstm = nn.LSTMCell(num_weekly_features, self.hidden_size)
+        self.fc = nn.Linear(self.hidden_size, 2)
         self.init_weights()
 
     def init_weights(self):
-        #TODO: initialize weights and biases of lstm cells according to specified distributions
+
+        for conv in [self.roster_scan_1, self.roster_scan_2]:
+            nn.init.xavier_uniform_(conv.weight, gain=1.0)
+            nn.init.constant_(conv.bias, 0.0)
         nn.init.xavier_uniform_(self.lstm.weight_ih, gain=1.0)
         nn.init.xavier_uniform_(self.lstm.weight_hh, gain=1.0)
         nn.init.constant_(self.lstm.bias_ih, 0.0)
         nn.init.constant_(self.lstm.bias_hh, 0.0)
-        ##
-        #TODO: initialize weights and biases of FC layer according to specified distributions
-        fc = self.fc
-        nn.init.xavier_uniform_(fc.weight, gain=1.0)
-        nn.init.constant_(fc.bias, 0.0)
+        nn.init.xavier_uniform_(self.fc.weight, gain=1.0)
+        nn.init.constant_(self.fc.bias, 0.0)
     
-    def forward(self, x):
-        N, T, d = x.shape
-        h_t, c_t = self.init_hidden(N)
-        for chunk in x.split(1,dim=1):
-            chunk = torch.squeeze(chunk,dim=1)
-            h_t, c_t = self.lstm(chunk,(h_t,c_t))
+    def forward(self, timeless, time):
+        batch, channels, width, height = timeless.shape
+        rosters = F.sigmoid(self.roster_scan_1(timeless))
+        rosters = F.sigmoid(self.roster_scan_2(rosters))
+        rosters = rosters.view(-1, self.hidden_size)
+        h_t = rosters.clone()
+        c_t = rosters.clone()
+        output = [self.fc(rosters)]
 
+        batch, num_weekly_features, length = time.shape
+        for chunk in time.split(1, dim=1):
+            chunk = torch.squeeze(chunk, dim=1)
+            h_t, c_t = self.lstm(chunk, (h_t, c_t))
+            output.append(self.fc(h_t))
 
-
-        z = self.fc(h_t)
-        z = F.sigmoid(z)
-        return z
+        return output
--- team.py
+++ team.py
@@ -9,6 +9,7 @@ class Team():
         with open('data/finalized_rosters{}.json'.format(year)) as f:
             our_dict = json.load(f)
         self.roster = {}
+        self.layers = []
         self.roster['QB'] = our_dict[name]['QB'] # should be a list of quarterbacks
         self.roster['RB'] = our_dict[name]['RB']
         self.roster['OL'] = our_dict[name]['OL']
@@ -24,7 +25,13 @@ class Team():
             for play in self.roster[pos]:
                 if 'rating' not in play:
                     play['rating'] = 0.7
-            players.sort(reverse=True, key=lambda player: player['rating']+player['year']/7)
+                if 'weight' not in play:
+                    play['weight'] = 160
+                if 'height' not in play:
+                    play['height'] = 70
+                if 'year' not in play:
+                    play['year'] = 1
+            players.sort(reverse=True, key=lambda player: player['rating'])
         self.clean_info({})
 
     def clean_info(self, params):
@@ -54,6 +61,27 @@ class Team():
                         self.roster[pos].pop(n)
                         self.remove_dupes()  # extremely lazy
                         return
+
+    def create_layers(self):
+        # Make the layers tensor a 120x2x2 image
+        self.layers = torch.zeros(120, 2, 2, dtype=float)
+        global_counter = 0
+        with open("data/ffnn.json") as f:
+            info = json.load(f)
+        for position, occurrences in info.items():
+            for i in range(occurrences):
+                try:
+                    print(self.roster[position][i]['first_name'], end=' ')
+                    print(self.roster[position][i]['last_name'], end=' ')
+                    print(self.roster[position][i]['rating'], end='\n')
+                    self.layers[global_counter+i][0][0] = self.roster[position][i]['rating']
+                    self.layers[global_counter + i][0][1] = self.roster[position][i]['year']
+                    self.layers[global_counter + i][1][0] = self.roster[position][i]['weight']
+                    self.layers[global_counter + i][1][1] = self.roster[position][i]['height']
+                except IndexError:
+                    global_counter += occurrences * 3
+
+
 
 
 if __name__ == "__main__":
